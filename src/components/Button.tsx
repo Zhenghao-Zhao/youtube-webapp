d7ee13c81d6afb90b895b5f904ae6555e9a45fc2
Zhenghao-Zhao <grekinaus@gmail.com>
2023-10-17 10:28:48 +0800

    added lucide-react library for its built-in icons, added menu icon and tool icons on the right side of the website

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,8 +5,8 @@ import { twMerge } from "tailwind-merge"
 const buttonStyles = cva(["transition-colors"], {
   variants: {
     variant: {
-      default: ["bg-secondary", "hover: bg-secondary-hover"],
-      ghost: ["hover: bg-gray-100"],
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-gray-100"],
     },
     size: {
       default: ["rounded", "p-2"],
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,24 +1,33 @@
 import Logo from "../assets/widgets/Logo";
-import Menu from "../assets/widgets/Menu";
+import Button from "../components/Button";
 import MenuBar from "../components/MenuBar";
 import SearchBar from "../components/SearchBar";
+import { Menu, Upload, Bell, User } from "lucide-react"
 
 export default function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between px-4 py-1">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button>
+        <Button id="button" variant="ghost" size="icon">
           <Menu />
-        </button>
+        </Button>
         <a href="/">
           <Logo />
         </a>
       </div>
-      <div className="flex items-center">
-        <SearchBar />
-      </div>
-      <div className="flex items-center">
-        <MenuBar />
+      <form>
+        
+      </form>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
       </div>
     </div>
   )
