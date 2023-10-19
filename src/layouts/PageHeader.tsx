e180a7881e17adf4fbe2569f4c7363c62ec5bbca
Zhenghao-Zhao <grekinaus@gmail.com>
2023-10-19 12:42:34 +0800

    improved responsiveness in header, added tailwind inline styles

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -3,32 +3,31 @@ import Logo from "../assets/widgets/Logo";
 import Voice from "../assets/widgets/Voice";
 import MenuBar from "../components/MenuBar";
 import SearchBar from "../components/SearchBar";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import IconButton from "../components/IconButton";
 import { Return } from "../assets/widgets/Icons";
 
 export default function PageHeader() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  return isOpen? (
-    <div className="flex p-1 items-center justify-between">
-      <IconButton handleClick={() => setIsOpen(false)}>
-        <Return />
-      </IconButton>
-      <SearchBar />
-    </div>
-  ) : (
-    <div className="flex justify-between px-4 py-1">
-      <div className="flex gap-4 items-center shrink-0">
+
+  return (
+    <div className="flex justify-between items-center px-4 py-1 gap-2">
+      <div className={isOpen? "hidden sm:flex gap-4 items-center shrink-0" : "flex gap-4 items-center shrink-0"}>
         <Menu />
         <a href="/">
           <Logo />
         </a>
       </div>
-      <div className="flex items-center justify-center gap-2 grow">
-        <SearchBar className="hidden sm:flex "/>
-        <Voice className="hidden sm:block"/>
+      { isOpen && 
+      <IconButton handleClick={() => setIsOpen(false)} className="sm:hidden">
+        <Return />
+      </IconButton>
+      }
+      <div className={isOpen? "flex items-center justify-center gap-2 grow" : "hidden sm:flex items-center justify-center gap-2 grow"}>
+        <SearchBar />
+        <Voice />
       </div>
-      <div className="flex items-center shrink-0">
+      <div className={isOpen? "hidden sm:flex items-center shrink-0" : "flex items-center shrink-0"}>
         <MenuBar setIsOpen={ setIsOpen }/>
       </div>
     </div> 
