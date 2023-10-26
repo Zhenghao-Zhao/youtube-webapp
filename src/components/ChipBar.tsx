541c772f69586f74aec7aa0820e9ca0edb61450f
Zhenghao-Zhao <grekinaus@gmail.com>
2023-10-26 18:46:59 +0800

    fixed right arrow not showing when resize chipbar

--- src/components/ChipBar.tsx
+++ src/components/ChipBar.tsx
@@ -5,12 +5,21 @@ import ArrowButton from "./ArrowButton"
 
 export default function ChipBar() {
   const listRef = useRef<HTMLDivElement>(null);
+  const sizeRef = useRef<HTMLDivElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
   const [showLeft, setShowLeft] = useState(false);
   const [showRight, setShowRight] = useState(false);
   const [selectedChip, setSelectedChip] = useState<string>(chipArray[0]);
   const TRANSLATE_DISTANCE = 200; // 200px per click on arrow button
 
   useEffect(() => {
+    const chipbar = sizeRef.current!;
+    const containerObserver = new ResizeObserver(entries => {
+      const container = entries[0].target;
+      containerRef.current!.style.setProperty('width', '' + container.clientWidth + 'px');
+    })
+
+    containerObserver.observe(chipbar);
     const element = listRef.current!;
 
     const observer = new ResizeObserver(entires => {
@@ -53,12 +62,14 @@ export default function ChipBar() {
   }
 
   return (
-    <div className="sticky flex items-center top-14 bg-white">
-      {showLeft && <ArrowButton handleClick={handleLeftClick} className="rotate-180 left-0"/>}
-      <div ref={listRef} className="overflow-x-hidden flex items-center h-10 scroll-smooth gap-3 text-sm">
-        { chips }
+    <div ref={sizeRef} className="h-14">
+      <div ref={containerRef} className="fixed flex items-center top-14 bg-white h-12">
+        {showLeft && <ArrowButton handleClick={handleLeftClick} className="rotate-180 left-0"/>}
+        <div ref={listRef} className="overflow-x-hidden flex items-center h-10 scroll-smooth gap-3 text-sm">
+          { chips }
+        </div>
+        {showRight && <ArrowButton handleClick={handleRightClick} />}
       </div>
-      {showRight && <ArrowButton handleClick={handleRightClick} />}
     </div>
   )
 }
